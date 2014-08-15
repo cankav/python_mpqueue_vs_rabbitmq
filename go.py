ed09d685a8726cfc880da53eff792d8085323839
Can Kavaklıoğlu <eposta@cankavaklioglu.name.tr>
2014-08-15 10:07:58 +0300

    SelectConnection test

--- go.py
+++ go.py
@@ -46,23 +46,53 @@ def rabbitmq_consumer():
     channel.basic_consume(rabbitmq_callback, queue='hello', no_ack=True)
     channel.start_consuming()
 
+# Step #3
+def on_open(connection):
+
+    connection.channel(on_channel_open)
+
+# Step #4
+def on_channel_open(channel):
+    for i in range(run_num):
+        channel.basic_publish('',
+                              'hello',
+                              json.dumps([ 1407919673695, 'USDTRY', 1.300, 5, 1.340, 3, 1.4, 0, 1.3, 1231231, 0.03, 0.034 ]),
+                              pika.BasicProperties(content_type='text/plain',
+                                                   delivery_mode=1))
+    connection.close()
+
+parameters = pika.URLParameters("amqp://guest:guest@localhost:5672/%2F")
+connection = pika.SelectConnection(parameters=parameters,
+                                   on_open_callback=on_open)
+run_num = pow(10, 5)
+
 def test_rabbitmq(run_num, message):
     p = Process(target=rabbitmq_consumer, args=())
     p.start()
 
-    conn = pika.BlockingConnection(pika.URLParameters("amqp://guest:guest@localhost:5672/%2F"))
-    channel = conn.channel()
-    channel.queue_declare(queue='hello')
-    for i in range(run_num):
-        channel.basic_publish(exchange='', routing_key='hello', body=json.dumps(message))
+    # blocking performance limits to ~300 messages per second
+    #conn = pika.BlockingConnection(pika.URLParameters("amqp://guest:guest@localhost:5672/%2F"))
+    #channel = conn.channel()
+    #channel.queue_declare(queue='hello')
+    #for i in range(run_num):
+    #    channel.basic_publish(exchange='', routing_key='hello', body=json.dumps(message))
+    #channel.basic_publish(exchange='', routing_key='hello', body='1')
 
-    channel.basic_publish(exchange='', routing_key='hello', body='1')
-
+    
+    try:
+        # Step #2 - Block on the IOLoop
+        connection.ioloop.start()
+    # Catch a Keyboard Interrupt to make sure that the connection is closed cleanly
+    except KeyboardInterrupt:
+        # Gracefully close the connection
+        connection.close()
+        # Start the IOLoop again so Pika can communicate, it will stop on its own when the connection is closed
+        connection.ioloop.start()
 
 
 
 def test():
-    run_num = pow(10, 4)
+
     print 'run number ' + str(run_num) + '\n'
     message = [ 1407919673695, 'USDTRY', 1.300, 5, 1.340, 3, 1.4, 0, 1.3, 1231231, 0.03, 0.034 ]
 
